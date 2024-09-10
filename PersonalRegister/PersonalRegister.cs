dd196d8bbc16f99ddb11f65d1442cafaf16cdafd
geekz <jjohan91@hotmail.com>
2024-09-10 15:17:18 +0200

    Uppgiften klar, ska kolla om jag kan implementera lite tester

---
+++ PersonalRegister/PersonalRegister.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalRegister
+{
+    internal class PersonalRegister
+    {
+        private string? name;
+        private int salary;
+
+        public string? getName()
+        {
+            return name;
+        }
+
+        public int getSalary()
+        {
+            return salary;
+        }
+
+        // kollar om salary kan convertas till int, annars är det fel (lönen ska ska vara nummer, inte sträng)
+        // samt att det ska vara över eller lika med 0
+        public static bool checkValidSalary(string salary)
+        {
+            if (int.TryParse(salary, out int parsedSalary))
+            {
+                return parsedSalary >= 0;
+            }
+            return false;
+        }
+
+        public static bool checkValidName(string name)
+        {
+            // i namnet ska det inte gå att inkludera siffror/whitespace, till exempel Johan1 eller Joh an1 :)
+            foreach (char c in name)
+            {
+                if(!char.IsLetter(c))
+                {
+                    return false;
+                }
+            }
+            if (!string.IsNullOrWhiteSpace(name)) {
+                return true;
+            }
+            return false;
+        }
+
+        public PersonalRegister(string? name, int salary)
+        {
+            this.name = name;
+            this.salary = salary;
+        }
+    }
+}
--- PersonalRegister/Program.cs
+++ PersonalRegister/Program.cs
@@ -4,7 +4,63 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<PersonalRegister> list = new List<PersonalRegister>();
+
+            Console.WriteLine("Personal register program");
+
+            while (true)
+            {
+                Console.WriteLine("Enter för att fortsätta, ESC för att avsluta");
+                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+
+                string? personal_name;
+                while (true)
+                {
+                    Console.Write("Ange namn: ");
+                    personal_name = Console.ReadLine();
+
+                    // checkValidName = kollar om det är valid namn, det är metod som ligger i PersonalRegister klass
+                    if (PersonalRegister.checkValidName(personal_name))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fel input, var vänligen och ange ett giltigt namn utan siffror eller whitespaces");
+                    }
+                }
+
+                int personal_salary;
+                while (true)
+                {
+                    Console.Write("Ange lön: ");
+                    string? salaryInput = Console.ReadLine();
+
+                    // checkValidSalary = kollar om det är valid lön, det är metod som ligger i PersonalRegister klass
+                    if (PersonalRegister.checkValidSalary(salaryInput))
+                    {
+                        personal_salary = int.Parse(salaryInput);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fel input, var vänligen och ange ett giltigt heltal för lönen");
+                    }
+                }
+
+                PersonalRegister pr = new PersonalRegister(personal_name, personal_salary);
+                list.Add(pr);
+
+            }
+
+            Console.WriteLine("\nRegistrerade personer:");
+            foreach (PersonalRegister pr in list)
+            {
+                Console.WriteLine($"Namn: {pr.getName()} lön: {pr.getSalary()}");
+            }
         }
     }
 }
